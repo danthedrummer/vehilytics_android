32b6f060abefbbbaaa3850ffc7419de1a1c088b6
danthedrummer <danthedrummerrof@gmail.com>
2018-05-03 15:28:19 +0100

    Splash Screen
    Added a splash screen simply displaying the logo and launching the login
    activity after 5 seconds. The logo can be tapped to immediately launch
    the next activity. The handler callbacks are properly removed when the
    app is closed to stop the next activity launching over other apps.
    
    My plan is to use this activity to get stored user data and skip the
    login if the credentials are still valid.

---
+++ app/src/main/java/com/ddowney/vehilytics/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.ddowney.vehilytics
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val launchIntent: Intent by lazy { Intent(baseContext, LoginActivity::class.java) }
+
+    private val runnable = Runnable {
+        startActivity(launchIntent)
+        finish()
+    }
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler.postDelayed(runnable, 5000)
+
+        splash_image.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            startActivity(launchIntent)
+            finish()
+        }
+
+        // TODO: Load user data from storage
+        // If there is anything stored then validate the login token and route
+        // to either the login or home screen as appropriate.
+    }
+
+    override fun onStop() {
+        super.onStop()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
